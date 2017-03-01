711f52bc0d85946f07d25a357647e8159e296ff1
Siddharth Suneel <ssuneel@dminc.com>
2017-03-01 17:02:24 +0530

    Health manager class added.

---
+++ WatchKitDemoApp/WatchKitDemoApp/Utilities/HealthManager/HealthManager.swift
@@ -0,0 +1,16 @@
+//
+//  HealthManager.swift
+//  WatchKitDemoApp
+//
+//  Created by Siddharth suneel on 01/03/17.
+//  Copyright © 2017 Siddharth suneel. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+class HealthManager {
+    
+    let healthStore:HKHealthStore = HKHealthStore()
+    
+}
