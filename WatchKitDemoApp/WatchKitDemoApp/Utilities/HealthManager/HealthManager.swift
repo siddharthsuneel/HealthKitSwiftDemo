379a6533c477d220429e60127f88260955dedc09
Siddharth Suneel <ssuneel@dminc.com>
2017-03-02 12:24:26 +0530

    Authorisation functions added in Health Manager.

--- WatchKitDemoApp/WatchKitDemoApp/Utilities/HealthManager/HealthManager.swift
+++ WatchKitDemoApp/WatchKitDemoApp/Utilities/HealthManager/HealthManager.swift
@@ -13,4 +13,42 @@ class HealthManager {
     
     let healthStore:HKHealthStore = HKHealthStore()
     
+    
+    func authoriseHealthKit() {
+        
+        if HKHealthStore.isHealthDataAvailable() {
+            
+            let readTypes:Set<HKObjectType> = readDataTypes()
+            let writeTypes:Set<HKSampleType> = writeDataTypes()
+            
+            //reuest authorisation
+            healthStore.requestAuthorization(toShare: writeTypes, read: readTypes, completion: { (success,error) -> Void in
+                if !success {
+                    print("You didn't allow HealthKit to access these read/write data types. In your app, try to handle this error gracefully when a user decides not to provide access. The error was: %@. If you're using a simulator, try it on a device.", error!)
+                }
+                
+                //TODO: update reuired UI elements here...
+            })
+        }
+    }
+    
+    //MARK: HealthKit Permissions
+    
+    func readDataTypes() -> Set<HKObjectType> {
+        
+        let stepsCount: HKQuantityType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!
+        
+        let readSet:Set = [stepsCount]
+        
+        return readSet
+    }
+    
+    func writeDataTypes() -> Set<HKSampleType> {
+        
+        let height: HKQuantityType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!
+        
+        let writeSet:Set = [height]
+        
+        return writeSet
+    }
 }
