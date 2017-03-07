cf1f7dc8f23562ff9090399be1db47b8988b16a5
Siddharth Suneel <ssuneel@dminc.com>
2017-03-07 11:33:57 +0530

    Home page UI implemented.

--- WatchKitDemoApp/WatchKitDemoApp/ViewControllers/ViewController.swift
+++ WatchKitDemoApp/WatchKitDemoApp/ViewControllers/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,39 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func setup() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
+    }
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = {
+            guard let cell1 = tableView.dequeueReusableCell(withIdentifier: Constants.tableViewCellIdentifier) else {
+                return UITableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier: Constants.tableViewCellIdentifier)
+            }
+            return cell1
+        }()
+        cell.selectionStyle = .none
+        cell.textLabel?.text = "Hello : \(indexPath.row)"
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
